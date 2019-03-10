11e78e3308069bd30896d577d509cc7427f1487f
JohnBro <lujunboo@gmail.com>
2019-03-10 19:46:45 +0800

    Add for lsp-ui-doc customize config.

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evil-want-C-i-jump t)
+ '(lsp-ui-doc-use-childframe nil)
  '(neo-window-width 40 t)
  '(package-selected-packages
    (quote
