ec0253ec6fa9be99fbde4c946736d82caa9cf331
JohnBro <lujunboo@gmail.com>
2019-03-10 17:01:16 +0800

    Add for using develop spacemacs BR.

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-C-i-jump t)
  '(neo-window-width 40 t)
  '(package-selected-packages
    (quote
